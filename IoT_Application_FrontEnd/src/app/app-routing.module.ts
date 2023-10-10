0b9d0166c614344e038e35fe2bd4aefaf0edda08
Sophisss <scattolinisofia26@gmail.com>
2023-10-10 11:24:22 +0200

    Add attributes for entity

--- IoT_Application_FrontEnd/src/app/app-routing.module.ts
+++ IoT_Application_FrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StructureComponent } from './structure/structure.component';
-import { EntityComponentComponent } from './structure/entity-component/entity-component.component';
 
 const routes: Routes = [
-{ path: '', component: StructureComponent },
-{ path: 'add_entity', component: EntityComponentComponent }
+{ path: '', component: StructureComponent }
 ];
 
 @NgModule({
--- IoT_Application_FrontEnd/src/app/structure/structure.component.ts
+++ IoT_Application_FrontEnd/src/app/structure/structure.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environments';
+import { Entity } from '../Models/Entity';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Configuration } from '../Models/Configuration';
+import { Field } from '../Models/Field';
 
 @Component({
   selector: 'app-structure',
@@ -7,9 +13,98 @@ import { Component } from '@angular/core';
 })
 export class StructureComponent {
 
-  click(){
-    console.log("Stampa")
+  responseData: any;
+
+  //This is the variable for the FormGroup.
+  form!: FormGroup;
+
+  entities: Entity[] = [];
+
+  configuration: Configuration = new Configuration;
+
+
+  showForm = false
+
+  entity!: Entity;
+
+  /**
+   * Constructor for this Component. 
+   * @param httpClient the HttpClient object.
+   */
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.initForm();
   }
 
+  //This is the method for reset the FormGroup value.
+  resetForm() {
+    this.form.reset();
+  }
+
+  //This is the method for reset the Data from backend.
+  resetData() {
+    this.responseData = null;
+  }
+
+  //This is the method for init the FormGroup. 
+  initForm() {
+    this.form = this.formBuilder.group({
+      nome: ['', Validators.required],
+      nomeAttributo: ['', Validators.required],
+      type: ['', Validators.required],
+      isrequired: ['', Validators.required]
+    });
+  }
+
+  openForm() {
+    const nome = this.form.value.nome;
+    this.entity = new Entity(nome);
+    this.configuration.entities.push(this.entity)
+    this.showForm = true
+    console.log("Entità aggiunta")
+  }
+
+  save() {
+    const nomeAttributo = this.form.value.nomeAttributo
+    const type = this.form.value.type
+    const isrequired = this.form.value.isrequired
+    const newField = new Field(nomeAttributo, type, isrequired)
+    this.entity.fields.push(newField)
+    this.resetForm()
+  }
+
+  closeAttributesForm() {
+    this.showForm = false;
+  }
+
+  export() {
+    const jsonEntities = this.configuration.entities.map(entity => (this.exportFields(entity)));
+    const jsonObject = {
+      entities: jsonEntities
+    };
+    this.resetForm()
+    console.log(jsonObject)
+  }
+
+  exportFields(entity: Entity) {
+    const jsonField = entity.fields.map(fields => ({
+      name: entity.entity_name,
+      fields: [{
+        name: fields.name,
+        type: fields.type,
+        required: fields
+      }]
+    }))
+    return jsonField
+  }
+
+
+
+  click() {
+    console.log("Stampa")
+  }
 
 }
