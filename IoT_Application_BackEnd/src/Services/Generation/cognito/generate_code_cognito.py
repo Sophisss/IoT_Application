ae48755930bc4472361d48ca68b6878644838e56
Sophisss <scattolinisofia26@gmail.com>
2023-11-14 17:55:15 +0100

    Refactore code to generate cognito resources template

--- IoT_Application_BackEnd/src/Services/Generation/cognito/generate_code_cognito.py
+++ IoT_Application_BackEnd/src/Services/Generation/cognito/generate_code_cognito.py
@@ -1,18 +1,44 @@
-import json
 import os
-import boto3
 from ruamel.yaml import YAML
-import utility_methods
-
-# Initialize the S3 client.
-s3_client = boto3.client('s3')
+import utility_methods as utilities
+from generate_header_templates import add_header_yaml_template
 
 
-# Function that update the Cognito-related CloudFormation template with new resources.
+def generate_template_cognito(bucket_name, folder_name):
+    """
+    This function generate the Cognito-related CloudFormation template.
+    :param bucket_name: S3 bucket name.
+    :param folder_name: S3 folder name.
+    :return: the Cognito-related CloudFormation template.
+    """
+    yaml = YAML()
+    file_name = 'permissions.yaml'
+    add_header_yaml_template(yaml, file_name, bucket_name, folder_name)
+    return add_to_resources_template_cognito(yaml, file_name, bucket_name, folder_name)
+
+
 def add_to_resources_template_cognito(yaml, file_name, bucket_name, folder_name):
+    """
+    This function update the Cognito-related CloudFormation template with new resources.
+    :param yaml: YAML object.
+    :param file_name: File name.
+    :param bucket_name: S3 bucket name.
+    :param folder_name: S3 folder name.
+    :return: Response.
+    """
     key = os.path.join(folder_name, file_name)
     template = yaml.load(utilities.get_s3_object(bucket_name, key)['Body'].read().decode('utf-8'))
+    add_resources_to_template(template)
 
+    response = utilities.put_s3_object(bucket_name, key, utilities.create_archive(yaml, template))
+    return utilities.handle_response(response, 'File updated successfully')
+
+
+def add_resources_to_template(template):
+    """
+    This function add new resources to the Cognito-related CloudFormation template.
+    :param template: Cognito-related CloudFormation template.
+    """
     if 'Resources' not in template:
         template['Resources'] = {}
 
@@ -21,18 +47,28 @@ def add_to_resources_template_cognito(yaml, file_name, bucket_name, folder_name)
     add_user_pool(json_data_cognito, template)
     add_identity_pool(json_data_cognito, template)
 
-    response = utilities.put_s3_object(bucket_name, key, yaml, template)
 
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        return {'statusCode': 200, 'body': 'File aggiornato con successo'}
-    else:
-        return {'statusCode': 500, 'body': json.dumps({
-            'message': 'Errore durante l\'aggiornamento del file'
-        })}
+def add_user_pool(json_data, template):
+    """
+    This function add a User Pool to the CloudFormation template.
+    :param json_data: the json data of the configuration file.
+    :param template: the CloudFormation template.
+    """
+    user_pool_name = json_data["UserPool"]["resource_name"]
+    password_policy = json_data["UserPool"]["policy"]["PasswordPolicy"]
+    template['Resources'][f"{user_pool_name}"] = {
+        "Type": "AWS::Cognito::UserPool",
+        "Properties": add_properties_user_pool_template(json_data, password_policy)
+    }
 
 
-# Function that add the user pool properties to the CloudFormation template.
-def add_properties_userpool_template(json_data, password_policy):
+def add_properties_user_pool_template(json_data, password_policy):
+    """
+    This function add the properties of the user pool to the CloudFormation template.
+    :param json_data: the json data of the configuration file.
+    :param password_policy: the password policy of the user pool.
+    :return: the properties of the user pool.
+    """
     return {
         "UserPoolName": json_data["UserPool"]["UserPoolName"],
         "Policies": add_policies_template(password_policy),
@@ -45,15 +81,23 @@ def add_properties_userpool_template(json_data, password_policy):
     }
 
 
-# Function that add the policies to the CloudFormation template.
 def add_policies_template(password_policy):
+    """
+    This function add the policies of the user pool to the CloudFormation template.
+    :param password_policy: the password policy of the user pool.
+    :return: the policies of the user pool.
+    """
     return {
         "PasswordPolicy": add_password_policy_template(password_policy)
     }
 
 
-# Function that add the password policy to the CloudFormation template.
 def add_password_policy_template(password_policy):
+    """
+    This function add the password policy to the CloudFormation template.
+    :param password_policy: the password policy of the user pool.
+    :return: the password policy of the user pool.
+    """
     return {
         "MinimumLength": password_policy["MinimumLength"],
         "RequireUppercase": password_policy["RequireUppercase"],
@@ -64,21 +108,25 @@ def add_password_policy_template(password_policy):
     }
 
 
-# Function that add a User Pool to the CloudFormation template.
-def add_user_pool(json_data, template):
-    user_pool_name = json_data["UserPool"]["resource_name"]
-    password_policy = json_data["UserPool"]["policy"]["PasswordPolicy"]
-    new_resource = {
-        f"{user_pool_name}": {
-            "Type": "AWS::Cognito::UserPool",
-            "Properties": add_properties_userpool_template(json_data, password_policy)
-        }
+def add_identity_pool(json_data, template):
+    """
+    This function add an Identity Pool to the CloudFormation template.
+    :param json_data: the json data of the configuration file.
+    :param template: the CloudFormation template.
+    """
+    identity_pool_name = json_data["IdentityPool"]["resource_name"]
+    template['Resources'][f"{identity_pool_name}"] = {
+        "Type": "AWS::Cognito::IdentityPool",
+        "Properties": add_properties_identity_pool_template(json_data)
     }
-    template['Resources'].update(new_resource)
 
 
-# Function that add the identity pool properties to the CloudFormation template.
-def add_properties_identitypool_template(json_data):
+def add_properties_identity_pool_template(json_data):
+    """
+    This function add the identity pool properties to the CloudFormation template.
+    :param json_data: the json data of the configuration file.
+    :return: the identity pool properties.
+    """
     return {
         "IdentityPoolName": json_data["IdentityPool"]["IdentityPoolName"],
         "AllowUnauthenticatedIdentities": False,
@@ -91,21 +139,5 @@ def add_properties_identitypool_template(json_data):
     }
 
 
-# Function that add an Identity Pool to the CloudFormation template.
-def add_identity_pool(json_data, template):
-    identity_pool_name = json_data["IdentityPool"]["resource_name"]
-    new_resource = {
-        f"{identity_pool_name}": {
-            "Type": "AWS::Cognito::IdentityPool",
-            "Properties": add_properties_identitypool_template(json_data)
-        }
-    }
-    template['Resources'].update(new_resource)
 
 
-# Function that generate the CloudFormation template.
-def generate_template_cognito(bucket_name, folder_name):
-    yaml = YAML()
-    file_name = 'permissions.yaml'
-    utilities.add_header_yaml_template(yaml, file_name, bucket_name, folder_name)
-    return add_to_resources_template_cognito(yaml, file_name, bucket_name, folder_name)
