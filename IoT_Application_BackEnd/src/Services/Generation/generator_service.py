e8c8e29f1d6d6d544b526b2e184a56c830c6092d
mtt-sudo <57330371+mtt-sudo@users.noreply.github.com>
2023-11-16 16:42:57 +0100

    generate graphql schema

--- IoT_Application_BackEnd/src/Services/Generation/generator_service.py
+++ IoT_Application_BackEnd/src/Services/Generation/generator_service.py
@@ -1,4 +1,5 @@
 from Services.Generation.DAL.generator_lambda_service import generate_lambda_code
+from Services.Generation.Schema_graphQL.generator_schema import generate_graphql_schema
 from Services.Generation.Templates.api.generate_lambda_template import generate_lambda_template
 from Services.Generation.Templates.cognito.generate_cognito_template_service import generate_cognito_template
 
@@ -12,5 +13,6 @@ def generate_code(json: dict) -> dict:
     codes_generated = {
         "cognito.yaml": generate_cognito_template(json['awsConfig']['authentication']['cognito']),
         "api.yaml": generate_lambda_template(json),
+        "schema.graphql": generate_graphql_schema(json),
         "lambda.py": generate_lambda_code(json)}
     return codes_generated
