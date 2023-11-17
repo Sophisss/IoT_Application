e5154771f8b4f30f335eb071bb629bc30eb13953
mtt-sudo <57330371+mtt-sudo@users.noreply.github.com>
2023-11-17 10:38:44 +0100

    Generate Lambda

--- IoT_Application_BackEnd/src/Services/Generation/DAL/lambdas_template.py
+++ IoT_Application_BackEnd/src/Services/Generation/DAL/lambdas_template.py
@@ -14,6 +14,7 @@ separator = '{separator}'
 single_entity_storage_keyword = '{single_entity_storage_keyword}'
 partition_key_table = '{partition_key}'
 sort_key_table = '{sort_key}'
+GSI = '{GSI_name}'
 """
 
 template_lambda_handler = """
@@ -29,24 +30,47 @@ template_method_for_get = """
         case '{name_method}':
              {primary_key} = event['arguments']['{primary_key}']
              {primary_key} = f"{name_entity}{{separator}}{{{primary_key}}}"
-             {option}
+             {option}"""
+option_template_for_get_second_entity_link = """
+             response = table.get_item(Key={{
+                 partition_key_table: {partition_key},
+                 sort_key_table: single_entity_storage_keyword
+                 }})
+             response = response['Item']
+             response['{partition_key}'] = response.pop(partition_key_table)
+             if '{first_entity}' in event['projection']:
+                response_s = table.query(
+                    IndexName= GSI,
+                    KeyConditionExpression=(
+                        Key(sort_key_table).eq({partition_key})
+                    )
+                )
+                item = response_s['Items'][0]
+                response_s = table.get_item(Key={{
+                        partition_key_table: item[partition_key_table],
+                        sort_key_table: single_entity_storage_keyword
+                    }})
+                response_s= response_s['Item']
+                response_s['{sort_key}'] = response_s.pop(partition_key_table)
+                response['Building'] = response_s"""
+option_template_for_get_without_link = """
              response = table.get_item(Key={{
                  partition_key_table: {primary_key},
                  sort_key_table: single_entity_storage_keyword
                  }})
              response = response['Item']
              response['{primary_key}'] = response.pop(partition_key_table)"""
-option_template_for_link = """
+option_template_for_get_first_entity_with_link = """
              if '{second_entity}' in event['projection']:
                 response = table.query(
                     KeyConditionExpression=(
-                        Key(partition_key_table).eq({primary_key})
+                        Key(partition_key_table).eq({partition_key})
                     )
                 )
                 result = []
                 for item in response['Items']:
                     if item[sort_key_table] == single_entity_storage_keyword :
-                        item['{primary_key}'] = item.pop(partition_key_table)
+                        item['{partition_key}'] = item.pop(partition_key_table)
                         response = item
                     else:
                         response_query = table.get_item(Key={{
@@ -54,9 +78,16 @@ option_template_for_link = """
                             sort_key_table: single_entity_storage_keyword
                         }})
                         response_query = response_query['Item']
-                        response_query['{primary_key}'] = response_query.pop(partition_key_table)
+                        response_query['{sort_key}'] = response_query.pop(partition_key_table)
                         result.append(response_query)
-                response['{second_entity}'] = result"""
+                response['{second_entity}'] = result
+             else:
+                 response = table.get_item(Key={{
+                 partition_key_table: {partition_key},
+                 sort_key_table: single_entity_storage_keyword
+                 }})
+             response = response['Item']
+             response['{partition_key}'] = response.pop(partition_key_table)"""
 
 template_method_for_delete = """
         case '{name_method}':
@@ -81,7 +112,7 @@ template_method_for_create = """
             response = arguments"""
 
 template_lambda_handler_link = """
-def lambda_handler_{first_entity}{second_entity}_link(event, context):
+def lambda_handler_{first_entity}{second_entity}(event, context):
     {partition_key} = event['arguments']['{partition_key}']
     {partition_key} = f"{first_entity}{{separator}}{{{partition_key}}}"
     {sort_key} = event['arguments']['{sort_key}']
@@ -95,19 +126,22 @@ def lambda_handler_{first_entity}{second_entity}_link(event, context):
 template_method_for_create_link = """
         case '{name_method}':
             arguments = event['arguments']
-            arguments[partition_key_table] = arguments.pop({partition_key})
-            arguments[sort_key_table] = arguments.pop({sort_key})
+            arguments[partition_key_table] = {partition_key}
+            arguments[sort_key_table] = {sort_key}
+            del arguments['{partition_key}']
+            del arguments['{sort_key}']
             response = table.put_item(Item=arguments)
             response = arguments"""
 template_method_for_get_link = """
         case '{name_method}':
-            response = table.put_item(Key={{
+            response = table.get_item(Key={{
                 partition_key_table: {partition_key},
                 sort_key_table: {sort_key}
             }})
             response = response['Item']
             response['{partition_key}'] = response.pop(partition_key_table)
-            response['{sort_key}'] = response.pop(sort_key_table)"""
+            response['{sort_key}'] = response.pop(sort_key_table)
+            response = list(response.values())"""
 template_method_for_delete_link = """
         case '{name_method}':
             response = table.delete_item(Key={{
@@ -116,4 +150,6 @@ template_method_for_delete_link = """
               }}, ReturnValues='ALL_OLD')
             response = response['Attributes']
             response['{partition_key}'] = response.pop(partition_key_table)
-            response['{sort_key}'] = response.pop(sort_key_table)"""
+            response['{sort_key}'] = response.pop(sort_key_table)
+            response = list(response.values())"""
+
