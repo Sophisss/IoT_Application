a07dd3517b0c48d42c202fd65be2a88451e56464
Sophisss <scattolinisofia26@gmail.com>
2023-11-24 16:20:27 +0100

    Refactor folder to generate code

--- IoT_Application_BackEnd/src/Services/Generation/generator_service.py
+++ IoT_Application_BackEnd/src/Services/Generation/generator_service.py
@@ -14,30 +14,30 @@ from Services.Generation.utility_methods import generate_resource_name
 def generate_code(json: dict) -> dict:
     """
     This function generate the code.
-    :param json: the json with the data.
+    :param json: the json containing the data.
     :return: the code generated.
     """
-    codes_generated = {}
-    __generate_code_template(codes_generated, json)
-    __generate_model_entity(codes_generated, json)
-    __generate_model_link(codes_generated, json)
-    __generate_code_graphql(codes_generated, json)
-    __generate_code_dal(codes_generated, json)
-    __generate_deployment_guide(codes_generated)
-    return codes_generated
+    code_generated = {}
+    __generate_template_code(code_generated, json)
+    __generate_entity_model(code_generated, json)
+    __generate_link_model(code_generated, json)
+    __generate_graphql_code(code_generated, json)
+    __generate_dal_code(code_generated, json)
+    __generate_deployment_guide(code_generated)
+    return code_generated
 
 
-def __generate_code_template(codes_generated: dict, json: dict):
+def __generate_template_code(codes_generated: dict, json: dict):
     """
-    This method generate the code for the template.
+    This method generate the code for the templates.
     :param codes_generated: the code that will be generated.
     :param json: the json with the data.
     """
-    codes_generated['template/cognito.yaml'] = generate_cognito_template(json['awsConfig']['authentication']['cognito'])
-    codes_generated['template/api.yaml'] = generate_api_template(json)
+    codes_generated['code_generated/template/cognito.yaml'] = generate_cognito_template(json['awsConfig']['authentication']['cognito'])
+    codes_generated['code_generated/template/api.yaml'] = generate_api_template(json)
 
 
-def __generate_model_entity(codes_generated: dict, json: dict):
+def __generate_entity_model(codes_generated: dict, json: dict):
     """
     This method generate the code for the model entity.
     :param codes_generated: the code that will be generated.
@@ -45,10 +45,10 @@ def __generate_model_entity(codes_generated: dict, json: dict):
     """
     for entity in json['entities']:
         entity_name = generate_resource_name(entity)
-        codes_generated[f'src/Model/{entity_name}.py'] = generate_model_entity(entity_name, entity['fields'])
+        codes_generated[f'code_generated/src/model/{entity_name}.py'] = generate_model_entity(entity_name, entity['fields'])
 
 
-def __generate_model_link(codes_generated: dict, json: dict):
+def __generate_link_model(codes_generated: dict, json: dict):
     """
     This method generate the code for the model link.
     :param codes_generated: the code that will be generated.
@@ -56,28 +56,28 @@ def __generate_model_link(codes_generated: dict, json: dict):
     """
     for link in json['links']:
         link_name = generate_resource_name(link)
-        codes_generated[f'src/Model/{link_name}.py'] = generate_model_link(link, json)
+        codes_generated[f'code_generated/src/model/{link_name}.py'] = generate_model_link(link, json)
 
 
-def __generate_code_graphql(codes_generated: dict, json: dict):
+def __generate_graphql_code(codes_generated: dict, json: dict):
     """
     This method generate the code for the graphql resources.
     :param codes_generated: the code that will be generated.
     :param json: the json with the data.
     """
-    codes_generated['src/graphql/schema.graphql'] = generate_graphql_schema(json)
-    codes_generated['src/graphql/invoker.js'] = generator_invoker()
+    codes_generated['code_generated/src/graphql/schema.graphql'] = generate_graphql_schema(json)
+    codes_generated['code_generated/src/graphql/invoker.js'] = generator_invoker()
 
 
-def __generate_code_dal(codes_generated: dict, json: dict):
+def __generate_dal_code(codes_generated: dict, json: dict):
     """
     This method generate the code for the dal resources.
     :param codes_generated: the code that will be generated.
     :param json: the json with the data.
     """
-    codes_generated['src/DynamoClass.py'] = generate_dbmanager(json)
-    codes_generated['src/lambda.py'] = generation_one_file(json)
-    codes_generated['src/ExceptionClasses.py'] = generator_exception()
+    codes_generated['code_generated/src/DynamoClass.py'] = generate_dbmanager(json)
+    codes_generated['code_generated/src/ExceptionClass.py'] = generator_exception()
+    codes_generated['code_generated/src/lambda.py'] = generation_one_file(json)
 
 
 def __generate_deployment_guide(codes_generated: dict):
@@ -85,4 +85,4 @@ def __generate_deployment_guide(codes_generated: dict):
     This method generate the code for the deployment guide.
     :param codes_generated: the code that will be generated.
     """
-    codes_generated['template/guide/deployment_guide.md'] = generate_deployment_guide()
+    codes_generated['code_generated/template/guide/deployment_guide.md'] = generate_deployment_guide()
