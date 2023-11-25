1b30928260b4dee40bd30f91d1027f855932950d
Christian Bonsignore <christian.bonsignore@studenti.unicam.it>
2023-11-25 02:21:54 +0100

    Base for the diagram page

--- IoT_Application_FrontEnd/src/app/app-routing.module.ts
+++ IoT_Application_FrontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomepageComponent} from "./components/homepage/homepage.component";
+import {WorkspaceComponent} from "./components/workspace/workspace.component";
 
 const routes: Routes = [
   {path: '', title: "Homepage", component: HomepageComponent},
+  {path: 'new', title: "New Project", component: WorkspaceComponent},
 
   {path: '**', redirectTo: ''}
 ];
--- IoT_Application_FrontEnd/src/app/app.module.ts
+++ IoT_Application_FrontEnd/src/app/app.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {HomepageComponent} from "./components/homepage/homepage.component";
+import {HeaderComponent} from "./components/header/header.component";
+import {WorkspaceComponent} from "./components/workspace/workspace.component";
 
 import {AppRoutingModule} from './app-routing.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from "@angular/material/button";
 import {MatTooltipModule} from "@angular/material/tooltip";
-import {HomepageComponent} from "./components/homepage/homepage.component";
 import {MatIconModule} from "@angular/material/icon";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -20,7 +22,9 @@ import {MatTableModule} from "@angular/material/table";
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
