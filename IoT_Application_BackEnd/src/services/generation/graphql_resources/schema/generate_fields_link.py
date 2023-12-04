149302dc1bea0c90705891bfd244f7c1e86fa1ae
mtt-vicari <mattia.vicari@studenti.unicam.it>
2023-12-04 09:20:17 +0100

    resolve problems

--- IoT_Application_BackEnd/src/services/generation/graphql_resources/schema/generate_fields_link.py
+++ IoT_Application_BackEnd/src/services/generation/graphql_resources/schema/generate_fields_link.py
@@ -6,18 +6,19 @@ type_mapping = {
 }
 
 
-def generate_fields_link(primary_key:dict, fields:list, type_partition_key:str, type_sort_key:str)->str:
+def generate_fields_link(primary_key: dict, fields: list, type_partition_key: str, type_sort_key: str) -> str:
     return (
             generate_fields_primary_key(primary_key[0], primary_key[1], type_partition_key, type_sort_key) +
             (''.join(map(generate_field, fields)) if fields else '')
     )
 
 
-def generate_fields_primary_key(partition_key:dict, sort_key:dict, type_partition_key:str, type_sort_key:str)->str:
+def generate_fields_primary_key(partition_key: dict, sort_key: dict, type_partition_key: str,
+                                type_sort_key: str) -> str:
     return f"""{partition_key}: {type_mapping.get(type_partition_key)}!
 {sort_key}: {type_mapping.get(type_sort_key)}!"""
 
 
-def generate_field(field:dict)->str:
+def generate_field(field: dict) -> str:
     return f"""
 {field['name']}: {type_mapping.get(field['type']) if 'allowedValues' not in field else f'allowed{field["name"]}'}{'!' if field['required'] else ''}"""
