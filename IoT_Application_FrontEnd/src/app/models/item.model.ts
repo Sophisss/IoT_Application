aa13ad890de0c8c76c7d489319c9ff827ea46345
Christian Bonsignore <christian.bonsignore@studenti.unicam.it>
2024-01-16 17:14:13 +0100

    Reformat

--- IoT_Application_FrontEnd/src/app/models/item.model.ts
+++ IoT_Application_FrontEnd/src/app/models/item.model.ts
@@ -2,32 +2,18 @@ import {Field} from "./field.model";
 
 export class Item {
   ID: number;
-
   name: string;
-
   type: 'entity' | 'table' | 'link';
-
   table: string;
-
   partition_key_name: string;
-
   partition_key_type: 'string' | 'integer';
-
   sort_key_name: string;
-
   sort_key_type: 'string' | 'integer'
-
   first_item_ID: number;
-
   second_item_ID: number;
-
   numerosity: 'one-to-one' | 'one-to-many' | 'many-to-many' | 'many-to-one';
-
   fields: Field[];
-
   primary_key: string[];
-
   keyword: string;
-
   separator: string;
 }
