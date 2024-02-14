685be1a63ed30355696e0c71a25810d06e9a08cd
Sophisss <scattolinisofia26@gmail.com>
2024-02-14 13:08:52 +0100

    Generazione DAL per archiviare i dati su timestream

---
+++ IoT_Application_BackEnd/src/services/generation/configuration_application/dal_resources/timeseries_manager/generator_project_timeseries_manager.py
@@ -0,0 +1,63 @@
+def generate_project_timeseries_manager(json_data: dict) -> str:
+    """
+    This function generates the timeseries manager class for the project.
+    :param json_data: The json data from the project configuration file.
+    :return: The timeseries manager class for the project.
+    """
+    return f"""{__generate_header()}
+{__generate_class(json_data)}
+    """
+
+
+def __generate_header() -> str:
+    """
+    This function generates the header of the timeseries manager class.
+    :return: The header of the timeseries manager class.
+    """
+    return """from timestream_manager.timeseries_dal import TimeseriesDal
+from iot.device_status_change import DeviceStatusChange
+    """
+
+
+def __generate_class(json_data: dict) -> str:
+    """
+    This function generates the timeseries manager class.
+    :param json_data: The json data from the project configuration file.
+    :return: The timeseries manager class.
+    """
+    return f"""class IoT_PlatformTimeseriesManager(TimeseriesDal):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(IoT_PlatformTimeseriesManager, cls).__new__(cls)
+        return cls._instance
+        
+{__generate_methods(json_data)}"""
+
+
+def __generate_methods(json_data: dict) -> str:
+    """
+    This function generates the methods of the timeseries manager class.
+    :param json_data: The json data from the project configuration file.
+    :return: The methods of the timeseries manager class.
+    """
+    return f"""{__generate_write_device_changes(json_data)}
+    """
+
+
+def __generate_write_device_changes(json_data: dict) -> str:
+    """
+    This function generates the write device changes method.
+    :param json_data: The json data from the project configuration file.
+    :return: The write device changes method.
+    """
+    database_name = json_data['awsConfig']['timestream']['database']['name']
+    table_name = json_data['awsConfig']['timestream']['table']['name']
+
+    return f"""    def write_device_changes(self, device_id: str, changes: [DeviceStatusChange]):
+        if not device_id or not changes or len(changes) <= 0:
+            raise ValueError("Invalid parameters")
+
+        self.write_device_status_changes(device_id, 'IoTPlatform-TimeStreamDB', 'IoTPlatform-DevicesTelemetry', changes)
+    """
---
+++ IoT_Application_BackEnd/src/services/generation/configuration_application/dal_resources/timeseries_manager/generator_timeseries_dal.py
@@ -0,0 +1,141 @@
+def generate_timeseries_dal() -> str:
+    """
+    This function generates TimeseriesDal class.
+    :return: The TimeseriesDal class generated.
+    """
+    return f"""{__generate_header()}
+{__generate_class()}
+    """
+
+
+def __generate_header() -> str:
+    """
+    This function generates the header of the TimeseriesDal class.
+    :return: The header of the TimeseriesDal class.
+    """
+    return """import boto3
+import time
+from iot.device_status_change import DeviceStatusChange
+    """
+
+
+def __generate_class() -> str:
+    """
+    This function generates the code for the TimeseriesDal class.
+    :return: The code for the TimeseriesDal class.
+    """
+    return f"""
+class TimeseriesDal:
+    timestream = None
+    timestream_write = None
+
+    def __init__(self):
+        self.timestream = boto3.client('timestream-query')
+        self.timestream_write = boto3.client('timestream-write')
+        
+{__generate_methods()}"""
+
+
+def __generate_methods() -> str:
+    """
+    This function generates the methods for the TimeseriesDal class.
+    :return: The methods for the TimeseriesDal class.
+    """
+    return f"""{__generate_write_device_status_changes()}
+{__generate_write_records()}
+{__generate_build_records()}
+{__generate_build_common_attributes()}
+{__generate_get_value_timestream_type()}
+    """
+
+
+def __generate_write_device_status_changes() -> str:
+    """
+    This function generates the write_device_status_changes method.
+    :return: The write_device_status_changes method.
+    """
+    return """    def write_device_status_changes(self, device_id: str, database_name: str, table_name: str, changes: [DeviceStatusChange]):
+        if not database_name or not table_name:
+            raise ValueError("Database and table names are required.")
+
+        records = self.__build_records(changes)
+        common_attributes = self.__build_common_attributes(device_id)
+        self.write_records(records, database_name, table_name, common_attributes)
+    """
+
+
+def __generate_write_records() -> str:
+    """
+    This function generates the method used to write records to Timestream.
+    :return: The method used to write records to Timestream.
+    """
+    return """    def write_records(self, records: list, database_name: str, table_name: str, common_attributes=None):
+        try:
+            if not records or len(records) <= 0:
+                raise ValueError("Empty records")
+
+            if common_attributes is None:
+                common_attributes = {}
+
+            result = self.timestream_write.write_records(DatabaseName=database_name,
+                                                         TableName=table_name,
+                                                         Records=records,
+                                                         CommonAttributes=common_attributes)
+
+        except Exception as error:
+            raise Exception(f"An error occurred while writing records to Timestream. {str(error)}")
+    """
+
+
+def __generate_build_records() -> str:
+    """
+    This function generates the method used to build records.
+    :return: The method used to build records.
+    """
+    return """    def __build_records(self, status_changes: [DeviceStatusChange]) -> [dict]:
+        records = []
+        for change in status_changes:
+            value = change.new_value
+            valueType = self.__get_value_timestream_type(value)
+            records.append({
+                'MeasureName': change.signal,
+                'MeasureValue': str(value),
+                'MeasureValueType': valueType
+            })
+
+        return records
+    """
+
+
+def __generate_build_common_attributes() -> str:
+    """
+    This function generates the method used to build common attributes.
+    :return: The method used to build common attributes.
+    """
+    return """    def __build_common_attributes(self, device_id: str) -> [dict]:
+        dimensions = [{'Name': 'device', 'Value': device_id}]
+
+        return {
+            "Dimensions": dimensions,
+            "Time": str(int(time.time() * 1000))
+        }
+    """
+
+
+def __generate_get_value_timestream_type() -> str:
+    """
+    This function generates the method used to get the value timestream type.
+    :return: The method used to get the value timestream type.
+    """
+    return """    @staticmethod
+    def __get_value_timestream_type(value):
+        valueType = type(value)
+        if valueType is str:
+            return 'VARCHAR'
+        elif valueType is bool:
+            return 'BOOLEAN'
+        elif valueType is int or valueType is float or valueType is Decimal:
+            return 'DOUBLE'
+        else:
+            return None
+    """
